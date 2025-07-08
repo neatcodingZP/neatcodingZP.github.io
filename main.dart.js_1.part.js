c2ff1ce9bd9914d64557d34cd969043120ca1721
Vova1408zp <neatcoding@gmail.com>
2025-07-08 18:15:08 +0300

    fix email link

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -2,18 +2,18 @@
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
-aGh(d,e,f){var x,w,v=null,u=new B.qq(C.pu),t=new B.qq(C.ip)
-t=new A.S4(u,t)
-t.M4(d,v,e,v)
+aGG(d,e,f){var x,w,v=null,u=new B.qw(C.px),t=new B.qw(C.ip)
+t=new A.Sk(u,t)
+t.Mb(d,v,e,v)
 x=t.a
 w=u.r
 w.toString
 x.r=w
-u.r=A.aVf(t)
+u.r=A.aVP(t)
 return t},
-aVd(d,e){switch(e){case"my_name":return"\u0412\u043e\u043b\u043e\u0434\u0438\u043c\u0438\u0440 \u0428\u0435\u0456\u043d"
+aVM(d,e){switch(e){case"my_name":return"\u0412\u043e\u043b\u043e\u0434\u0438\u043c\u0438\u0440 \u0428\u0435\u0456\u043d"
 case"greeting.hello":return"\u041f\u0440\u0438\u0432\u0456\u0442!"
-case"greeting.main_info":d.gSd()
+case"greeting.main_info":d.gSl()
 return y.c
 case"greeting.get_in_touch":return"\u0417\u0432'\u044f\u0437\u0430\u0442\u0438\u0441\u044f"
 case"background.title":return"\u041f\u0440\u043e \u043c\u0435\u043d\u0435"
@@ -21,42 +21,42 @@ case"background.sections.0.0":return"\u041f\u0435\u0440\u0448\u0435 <bold>senten
 case"background.sections.0.1":return"\u0414\u0440\u0443\u0433\u0435 sentence."
 case"background.sections.0.2":return"\u0422\u0440\u0435\u0442\u0454 sentence."
 default:return null}},
-aVf(d){return new A.avH(d)},
-S4:function S4(d,e){var _=this
+aVP(d){return new A.aw1(d)},
+Sk:function Sk(d,e){var _=this
 _.e=d
 _.r=_.f=$
 _.a=e
 _.c=_.b=$},
-auS:function auS(d,e){this.b=d
+avd:function avd(d,e){this.b=d
 this.a=e},
-avH:function avH(d){this.a=d}}
+aw1:function aw1(d){this.a=d}}
 B=c[0]
 C=c[2]
 A=a.updateHolder(c[3],A)
-A.S4.prototype={
-i(d,e){var x=this.e.C3(e)
-return x==null?this.a.C3(e):x},
-gSd(){var x=this.f
+A.Sk.prototype={
+h(d,e){var x=this.e.C9(e)
+return x==null?this.a.C9(e):x},
+gSl(){var x=this.f
 if(x===$){x!==$&&B.N()
 x=this.f=this}return x},
-gIS(){return"\u0412\u043e\u043b\u043e\u0434\u0438\u043c\u0438\u0440 \u0428\u0435\u0456\u043d"},
-gwi(){var x,w=this,v=w.r
-if(v===$){x=w.gSd()
+gIW(){return"\u0412\u043e\u043b\u043e\u0434\u0438\u043c\u0438\u0440 \u0428\u0435\u0456\u043d"},
+gwm(){var x,w=this,v=w.r
+if(v===$){x=w.gSl()
 w.r!==$&&B.N()
-v=w.r=new A.auS(x,x)}return v},
-grq(){return this.e}}
-A.auS.prototype={
-gWO(){return"\u041f\u0440\u0438\u0432\u0456\u0442!"},
-gXx(){return y.c},
-gKo(){return"\u0417\u0432'\u044f\u0437\u0430\u0442\u0438\u0441\u044f"}}
+v=w.r=new A.avd(x,x)}return v},
+gru(){return this.e}}
+A.avd.prototype={
+gWV(){return"\u041f\u0440\u0438\u0432\u0456\u0442!"},
+gXF(){return y.c},
+gKu(){return"\u0417\u0432'\u044f\u0437\u0430\u0442\u0438\u0441\u044f"}}
 var z=a.updateTypes([])
-A.avH.prototype={
-$1(d){return A.aVd(this.a,d)},
-$S:71};(function inheritance(){var x=a.inherit
-x(A.S4,B.hh)
-x(A.auS,B.S3)
-x(A.avH,B.kz)})()
-B.aHg(b.typeUniverse,JSON.parse('{"S4":{"hh":[],"ih":["fW","hh"]}}'))
+A.aw1.prototype={
+$1(d){return A.aVM(this.a,d)},
+$S:70};(function inheritance(){var x=a.inherit
+x(A.Sk,B.hi)
+x(A.avd,B.Sj)
+x(A.aw1,B.kA)})()
+B.aHE(b.typeUniverse,JSON.parse('{"Sk":{"hi":[],"ii":["fX","hi"]}}'))
 var y={c:"\u041c\u0435\u043d\u0435 \u0437\u0432\u0430\u0442\u0438 <bold>\u0412\u043e\u043b\u043e\u0434\u0438\u043c\u0438\u0440 \u0428\u0435\u0456\u043d</bold>, \u044f \u0440\u043e\u0437\u0440\u043e\u0431\u043d\u0438\u043a \u043c\u043e\u0431\u0456\u043b\u044c\u043d\u0438\u0445 \u0437\u0430\u0441\u0442\u043e\u0441\u0443\u043d\u043a\u0456\u0432 \u0437 \u0444\u043e\u043a\u0443\u0441\u043e\u043c \u043d\u0430 \u043a\u0440\u043e\u0441\u0441-\u043f\u043b\u0430\u0442\u0444\u043e\u0440\u043c\u0443 \u0442\u0430 \u0446\u0456\u043a\u0430\u0432\u0456 \u0434\u0435\u0442\u0430\u043b\u0456"}};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"cs04GMo7PDcAKzVrhhnzHM6Joks=");
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"ZOA4h+iCQ8+GNeSPrg/pJPvLB0I=");
